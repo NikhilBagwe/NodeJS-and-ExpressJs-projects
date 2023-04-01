bc21253696f18894d63464eeb062265d70d2c2f8
NikhilBagwe <67143015+NikhilBagwe@users.noreply.github.com>
2023-04-01 11:33:11 +0530

    Add files via upload

---
+++ express-CRUD/mycontacts-backend/routes/contactRoutes.js
@@ -0,0 +1,58 @@
+const express = require("express")
+const router = express.Router()
+const {
+  getContacts,
+  createContact,
+  getContact,
+  updateContact,
+  deleteContact,
+} = require("../controllers/contactController")
+
+// Simplification - 1 ===========================
+/*
+Below logic is put in controller.js file
+
+router.route("/").get((req, res) => {
+  //res.send("Get all contacts")
+  res.status(200).json({ message: "Get all contacts" })
+})
+router.route("/").post((req, res) => {
+  res.status(200).json({ message: "Create contact" })
+})
+
+router.route("/:id").put((req, res) => {
+  res.status(200).json({ message: `Update contact for ${req.params.id}` })
+})
+
+router.route("/:id").delete((req, res) => {
+  res.status(200).json({ message: `Delete contact for ${req.params.id}` })
+})
+
+// Get a particular contact
+router.route("/:id").get((req, res) => {
+  res.status(200).json({ message: `Get contact for ${req.params.id}` })
+})
+*/
+
+// Simplification - 2 ==============================
+
+/*Since below 2 routes are similar, we can even simplify them more
+
+router.route("/").get(getContacts)
+
+router.route("/").post(createContact)
+
+Similarly we can do same for '/:id' routes
+
+router.route("/:id").put(updateContact)
+
+router.route("/:id").delete(deleteContact)
+
+// Get a particular contact
+router.route("/:id").get(getContact)
+*/
+router.route("/").get(getContacts).post(createContact)
+
+router.route("/:id").put(updateContact).delete(deleteContact).get(getContact)
+
+module.exports = router
---
+++ express-CRUD/mycontacts-backend/server.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const connectDb = require("./config/dbConnection")
+const errorHandler = require("./middleware/errorHandler")
+const dotenv = require("dotenv").config()
+
+connectDb() //connect DB
+const app = express()
+const port = process.env.PORT
+
+// Body parser middleware - to parse data received from client to server side
+app.use(express.json())
+
+// adding middleware for routing
+app.use("/api/contacts", require("./routes/contactRoutes"))
+
+app.use(errorHandler)
+
+app.listen(port, () => console.log(`Server running on port ${port}`))
